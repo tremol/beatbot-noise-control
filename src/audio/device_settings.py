4b8afd914c452a1722b9cc69732ff119573240fb
tremol <paul.riggins@gmail.com>
2024-01-08 12:46:24 -0500

    require input device selection when prompting

--- src/audio/device_settings.py
+++ src/audio/device_settings.py
@@ -3,32 +3,31 @@
 import sounddevice as sd
 
 
-def get_samplerate(my_device):
-    samplerate = sd.query_devices(my_device, 'input')['default_samplerate']
+def get_samplerate(device):
+    samplerate = sd.query_devices(device, 'input')['default_samplerate']
     return samplerate
 
+def is_input_device(device):
+    try:
+        sd.check_input_settings(device)
+        return True
+    except sd.PortAudioError:
+        print('Selected device is not an input device.')
+        return False
 
 def prompt_device_selection():
     """ Prompt user to select input device. Default: 0. """
 
     available_devices = sd.query_devices()
-    valid_numbers = list(range(len(available_devices)))
 
     while True:
         print('Your audio devices are:')
         print(available_devices)
         print('Enter the number of your preferred input device [0]:')
         try:
-            device = input()
-            if device == '':
-                device = 0
-
-            device = int(device)
-            if device in valid_numbers:
+            device = int(input() or "0")
+            if is_input_device(device):
                 break
-            else:
-                raise ValueError
-        # triggers if invalid number entered, or if not a number entered
         except ValueError:
             print('Not a valid device. Enter only the device number (e.g., 0).')
 
